d15fc4c8f99876b134491e5b94f86cec5d2620fa
Xie Yuheng <xyheme@gmail.com>
2020-10-19 17:59:49 +0800

    up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,24 @@
 <script lang="ts">
   import { Component, Vue } from "vue-property-decorator"
   import HelloWorld from "@/components/HelloWorld.vue"
-  import * as cicada from "@xieyuheng/cicada"
-  console.log(cicada)
+  import { lang3 } from "@xieyuheng/cicada"
+
+  const text = `
+@datatype List : (Type) -> Type {
+  null: (T: Type) -> List(T)
+  cons: (T: Type) -> (head: T) -> (tail: List(T)) -> List(T)
+}
+
+@show List
+@show List.null
+@show List.cons
+@show List.null(String)
+@show List.cons(String)("abc")(List.null(String))
+  `
+
+  const tops = lang3.frontend.parse_tops(text)
+  const mod = lang3.Mod.init()
+  lang3.Top.run(mod, tops)
 
   @Component({
     components: {
