5e0de69eae8cf26492282665b8169263e689ad3b
Xie Yuheng <xyheme@gmail.com>
2020-10-19 17:24:40 +0800

    up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome" />
   </div>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from "vue-property-decorator"
-  import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
+  import HelloWorld from "@/components/HelloWorld.vue"
 
   @Component({
     components: {
