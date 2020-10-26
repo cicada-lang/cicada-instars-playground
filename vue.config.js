940f159bea6b0c11858fb33c5337462da0010897
Xie Yuheng <xyheme@gmail.com>
2020-10-26 22:18:11 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,14 @@ const path = require("path")
 
 module.exports = {
   configureWebpack: {
-    resolve: { alias: { "@": path.join(__dirname, "web") } },
-    entry: { app: path.join(__dirname, "web", "index.ts") },
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "web"),
+      },
+    },
+    entry: {
+      app: path.join(__dirname, "web", "index.ts"),
+    },
     module: {
       rules: [
         {
